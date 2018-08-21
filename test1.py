data trimming error fixed; efficiency calculation improved

test1.py
@@ -6,6 +6,7 @@ Created on Mon Aug 20 22:28:11 2018
 """
 
 import pandas as pd
+import numpy as np
 
 
 
@@ -27,12 +28,14 @@ def conv_to_sec(time_split):
 
 
     
-df = pd.read_csv("d1-n-data.csv", sep = ",")
+df = pd.read_csv("re_data.csv", sep = ",")
 
 time_split = []
 time_in_sec = []
 
-len_data = 2051
+len_data = 2055
+
+
 
 
 for i in range(0, len_data):
@@ -52,34 +55,43 @@ for i in range(0, len_data):
     
     time_in_sec.append(t1-t0)
 
+
+
+fuel = list(df["Fuel.Level"][:len_data])
+vspeed = list(df["Vehicle.Speed"][:len_data])
+
+
+
+i = 0
+while i < len_data:
+    if np.isnan(fuel[i]) or np.isnan(vspeed[i]):
+        print (i, "nan")
+        vspeed = vspeed[:i] + vspeed[i+1:]
+        fuel = fuel[:i] + fuel[i+1:]
+        time_in_sec = time_in_sec[:i] + time_in_sec[i+1:]
+        len_data = len_data - 1
+        i = i -1
+    i = i + 1
+
+
+
+print (vspeed[1230])  
+print (len_data)
+
+  
 dist = [0]
 for i in range(1, len_data):
     tdiff = time_in_sec[i] - time_in_sec[i-1]
-    speed = float(df["Vehicle Speed"][i])
-    speed0 = float(df["Vehicle Speed"][i-1])
+    speed = vspeed[i]
+    speed0 = vspeed[i-1]
     
     dist.append(dist[i-1] + tdiff*(speed+speed0)/2)
     
-timeinsec = pd.Series(time_in_sec)
-distances = pd.Series(dist)
-fuel = list(df["Fuel Level"][:len_data])
-vspeed = list(df["Vehicle Speed"][:len_data])
+ 
 
-#except NODATA in fuellevel 
-daterror = []
-i = 0
-while i < len_data:
-    try:
-        float(fuel[i][:-1])
-        fuel[i] = fuel[i][:-1]
-        i = i + 1
-    except ValueError:
-        timeinsec = timeinsec[:i] + timeinsec[i+1:]
-        del dist[i]
-        del vspeed[i]
-        del fuel[i]
-        len_data = len_data - 1
-    
+distances = pd.Series(dist)
+
+timeinsec = pd.Series(time_in_sec)
 
 timedist = pd.DataFrame({'Time(sec)' : timeinsec,
                          'Dist': dist,

test3.py
@@ -14,14 +14,36 @@ import pandas as pd
 
 dlen = 2044
 '''
-def longestdecsubs(list):
-    P = []
-    M = []
+def longestdecsubs(X, l):
+    P = np.zeros(l, 1)
+    M = np.zeros(l, 1)
 
     L = 0
     for i in range(dlen):
         #binary search for the smallest positive
-        L = 0
+        lo = 1
+        hi = L
+        while lo <= hi:
+            mid = ceil((lo+hi)/2)
+            if X[M[mid]] < X[i]:
+                lo = mid + 1
+            else:
+                hi = mid - 1
+
+        newL = lo
+
+        P[i] = M[newL-1]
+        M[newL] = i
+
+        if newL > L:
+            L = newL
+
+        result = []
+        k = M[L]
+        i = L-1
+        while i >= 0:
+            result = X[k] + result
+            k = P[k]
 '''
 
 #avg of self and neighboor
@@ -42,6 +64,7 @@ d1 = pd.read_csv("data1.csv", sep = ",")
 
 fuel = np.array(d1["Fuel Level"])
 dist = np.array(d1["Dist"])
+speed = np.array(d1["Vehicle speed"])
 
 eff = []
 
@@ -62,10 +85,82 @@ for i in range(0, dlen-1):
 fuel_1 = fuel[:partition[0]]
 fuel_2 = fuel[partition[0]:partition[1]]
 fuel_3 = fuel[partition[1]:]
-
+'''
 fuel_1_smt = smoother(fuel_1, partition[0])
 fuel_2_smt = smoother(fuel_2, partition[1])
 fuel_3_smt = smoother(fuel_3, partition[2])
+'''
+
+i = -1
+stoplist = []
+while i < partition[0]:
+    i = i + 1
+    if speed[i] == 0:
+        cnt = 0
+        start = i
+        i = i + 1
+        while speed[i] == 0:
+            i = i + 1
+            cnt = cnt + 1
+            end = i
+        if cnt > 5:
+            stoplist.append([start, end])
+            
+            
+print (stoplist)
+
+startdist = dist[0]
+startfuel = fuel_1[0]
+
+for pair in stoplist:
+    nowfuel = fuel_1[pair[0]]
+    nowdist = dist[pair[0]]
+    
+    noweff = (startfuel - nowfuel) / (nowdist - startdist)
+    
+    eff.append(noweff)
+    
+    startfuel = fuel_1[pair[1]]
+    startdist = dist[pair[1]]
+    
+    
+print (eff)
+
+print ("----------")
+startdist = dist[0]
+startfuel = fuel_1[0]
+
+partition.append(len_data-1)
+for time in partition:
+    fueldiff = startfuel - fuel[time-1]
+    distdiff = dist[time-1]- startdist
+    bigeff = (fueldiff/ distdiff)
+    print (startfuel, fuel[time-1], startdist, dist[time-1])
+    print (time, partition, fueldiff, distdiff, bigeff)
+    startfuel = fuel[time]
+    startdist = dist[time]
+    
+'''
+i = -1
+distorig = dist[0]
+fuelorig = fuel_1[0]
+while i < partition[0]:
+    i = i + 1
+    if speed[i] == 0:
+        sfuel = fuelorig - fuel_1[i]
+        sdist = dist[i]- distorig
+        print ("Fuel \t", sfuel)
+        print ("Dist \t", sdist)
+        if sdist != 0:
+            seff = sfuel/sdist
+        eff.append(seff)
+        print ("Efficiency \t", seff)
+        while speed[i] == 0:
+            i = i + 1
+            distorig = dist[i]
+            fuelorig = fuel_1[i]
 
 
+print (eff)
 
+'''